e23ee31fdefbd08ef4657567305e5bc4db7c6d13
Carlos eduardo <duducp2013@gmail.com>
2017-04-21 20:37:13 -0300

    Adicionado o modo de compreensão Gzip

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var compression = require('compression');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
@@ -18,5 +19,6 @@ app.use(bodyParser.urlencoded({extended: false})); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
 app.use(logger('dev')); // Log requests to API using morgan
 app.use(cors());
+app.use(compression());
 
 router(app);
