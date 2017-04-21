20180df3855fd99cb7b127813052c32182fc62f2
Carlos eduardo <duducp2013@gmail.com>
2017-04-21 08:53:19 -0300

    Adicionado variáveis para guardar os dados de conexão

--- config/database.js
+++ config/database.js
@@ -1,4 +1,10 @@
+const dbuser = 'dudu';
+const dbpassword = 'edinei6';
+const database = 'teste-jwt';
+const port = '61210';
+const url_server = 'ds161210.mlab.com';
+
 module.exports = {
-    //'url': 'mongodb://dudu:edinei6@ds161210.mlab.com:61210/teste-jwt'
-    'url': 'mongodb://localhost/neppo'
+    'url': 'mongodb://' + dbuser + ':' + dbpassword + '@' + url_server + ':' + port + '/' + database
+    //'url': 'mongodb://localhost/neppo'
 };
