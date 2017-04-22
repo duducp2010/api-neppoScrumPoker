2b190ca6e87d531cede20b27d07bb98290dc517f
Carlos Eduardo <dudu.oo@outlook.com>
2017-04-22 19:14:13 -0300

    Adicionado suporte para versões de rotas.

--- app/routes.js
+++ app/routes.js
@@ -10,34 +10,39 @@ const requireAuth = passport.authenticate('jwt', {session: false}),
 
 module.exports = function (app) {
 
-    const apiRoutes = express.Router(),
+    const versionApi = express.Router(),
+        apiRoutes = express.Router(),
         authRoutes = express.Router(),
         userRoutes = express.Router(),
         projectRoutes = express.Router();
 
-    // Auth Routes
-    apiRoutes.use('/auth', authRoutes);
-
-    authRoutes.post('/register', AuthenticationController.register);
-    authRoutes.post('/login', requireLogin, AuthenticationController.login);
-    authRoutes.get('/protected', requireAuth, function (req, res) {
-        res.send({content: 'Success'});
-    });
-
-    // User Routes
-    apiRoutes.use('/user', userRoutes);
-    userRoutes.get('/', requireAuth, UserController.getAllUsers);
-    userRoutes.get('/:id_user', requireAuth, UserController.getUser);
-
-    // Project Routes
-    apiRoutes.use('/project', projectRoutes);
-    projectRoutes.get('/', requireAuth, ProjectController.getAllProjects);
-    projectRoutes.get('/:id_project', requireAuth, ProjectController.getProject);
-    projectRoutes.post('/', requireAuth, ProjectController.create);
-    projectRoutes.put('/:id_project', requireAuth, ProjectController.update);
-    projectRoutes.delete('/:id_project', requireAuth, ProjectController.delete);
-
     // Set up routes
-    app.use('/api', apiRoutes);
+    app.use('/api', versionApi);
 
+    // Api V1
+    versionApi.use('/v1',
+        // Auth Routes
+        apiRoutes.use('/auth',
+            authRoutes.post('/register', AuthenticationController.register),
+            authRoutes.post('/login', requireLogin, AuthenticationController.login),
+            authRoutes.get('/protected', requireAuth, function (req, res) {
+                res.send({content: 'Success'});
+            })
+        ),
+
+        // User Routes
+        apiRoutes.use('/user',
+            userRoutes.get('/', requireAuth, UserController.getAllUsers),
+            userRoutes.get('/:id_user', requireAuth, UserController.getUser)
+        ),
+
+        // Project Routes
+        apiRoutes.use('/project',
+            projectRoutes.get('/', requireAuth, ProjectController.getAllProjects),
+            projectRoutes.get('/:id_project', requireAuth, ProjectController.getProject),
+            projectRoutes.post('/', requireAuth, ProjectController.create),
+            projectRoutes.put('/:id_project', requireAuth, ProjectController.update),
+            projectRoutes.delete('/:id_project', requireAuth, ProjectController.delete)
+        )
+    );
 };
