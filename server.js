ef695de1675454aa2148804852190655ab90adf5
Carlos eduardo <duducp2013@gmail.com>
2017-04-23 17:16:04 -0300

    Arquivo configurado para melhor compreenção

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+/**
+ * Arquivo: server.js
+ * Descrição: Arquivo responsável por levantar o serviço do Node.Js para poder
+ * executar a aplicação e a API através do Express.Js.
+ * Author:
+ * Data de Criação: 20/04/2017
+ */
+
 const express = require('express');
 const app = express();
 const compression = require('compression');
@@ -5,15 +13,10 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const databaseConfig = require('./config/database');
+const dbConfig = require('./config/database');
 const router = require('./app/routes');
 
-mongoose.connect(databaseConfig.url);
-
 const port = process.env.PORT || 8080;
-app.listen(port);
-console.log("Servidor rodando na porta: " + port );
 
 app.use(bodyParser.urlencoded({extended: false})); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
@@ -21,4 +24,9 @@ app.use(logger('dev')); // Log requests to API using morgan
 app.use(cors());
 app.use(compression());
 
+mongoose.connect(dbConfig.url);
 router(app);
+
+//Iniciando o Servidor (Aplicação)
+app.listen(port);
+console.log("Servidor rodando na porta: " + port);
